602b8aae0315cd066736244f7f75dc0562036692
boy.wang <boyw165@gmail.com>
2014-04-03 19:26:38 +0800

    update.

--- oops-mode.el
+++ oops-mode.el
@@ -1,46 +1,15 @@
-
-;; [buffer-local] noob-mode
-(make-variable-buffer-local
- (defvar noob-mode nil
-   "noob-mode is a buffer-local variable.")
- )
-
 ;; [buffer-local] list containing definition windows.
 (make-variable-buffer-local
- (defvar def-windows nil
+ (defvar oops-def-window nil
    "Definition window that showing the definition. It's a buffer-local variable.")
  )
 
-(defun init-def-windows ()
-  "Create definition windows refer to how many valid frames."
-  (frame-list)
-  )
-
 ;;;###autoload
-(define-minor-mode noob-mode
+(define-minor-mode oops-mode
   "Constructing..."
-  :lighter " noob")
-
-;;;###autoload
-(add-hook 'emacs-lisp-mode-hook 'noob-mode)
-
-(provide 'noob-mode)
-
-;; frame remove hook to remove definition window
-
-(setq aaa1 t)
-(defun aaa1 ()
-  "asdfasdfadsf"
-  (interactive)
-  (if aaa1
-      ; if
-      (progn
-        (message)
-        )
-  ; else
-  (progn
-    (message)
-    )
+  :lighter " Oops"
+  (message "%s" oops-mode)
   )
 
-(setq win1 (split-window nil nil 'below))
+(add-hook 'emacs-lisp-mode 'oops-mode)
+(add-hook 'lisp-interaction-mode 'oops-mode)
