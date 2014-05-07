08bb6c8c9336658adda739c02b5ca9c4089650f4
Boy.Wang <boyw165@gmail.com>
2014-05-08 00:29:27 +0800

    Support help for elisp built-in variable.

--- oops/jotting.el
+++ oops/jotting.el
@@ -45,15 +45,17 @@
                    alias 'variable-documentation)))
          (val (symbol-value variable))
          (locus (variable-binding-locus variable))
-         (permanent-local (get variable 'permanent-local))
          (file-name (find-lisp-object-file-name variable 'defvar))
+         (permanent-local (get variable 'permanent-local))
+         (obsolete (get variable 'byte-obsolete-variable))
+         (safe-var (get variable 'safe-local-variable))
          (print-length 32)
          (print-level 3)
          )
     ;; TODO: Make variable text with hyper-link property.
 
-    ;; Indicate where is its definition and its current value.
-    (princ variable)
+    ;; Indicate where is its definition and current value.
+    (princ (format "`%s'" variable))
     (princ " is a variable defined in ")
     (princ (if (eq file-name 'C-source)
                "C source code"
@@ -126,131 +128,122 @@
       (princ "\n  Automatically becomes ")
       (if permanent-local
           (princ "permanently "))
-      (princ "buffer-local when set.\n\n")
+      (princ "buffer-local when set.")
       )
      ((not permanent-local))
      ((bufferp locus)
-      (princ "\n  This variable's buffer-local value is permanent.\n\n")
+      (princ "\n  This variable's buffer-local value is permanent.")
       )
      (t
-      (princ "\n  This variable's value is permanent if it is given a local binding.\n\n"))
+      (princ "\n  This variable's value is permanent if it is given a local binding."))
      )
 
-    (error "stop.")
+    ;; Indicate it is a alias.
+    (unless (eq alias variable)
+      (princ (format "\n  This variable is an alias for `%s'." alias))
+      )
 
-    ;; Print message like following:
-    ;; "Automatically becomes buffer-local when set?"
-    (let* ((obsolete (get variable 'byte-obsolete-variable))
-           (use (car obsolete))
-           (safe-var (get variable 'safe-local-variable))
-           (extra-line nil))
-
-      ;; Mention if it's an alias.
-      (unless (eq alias variable)
-        (setq extra-line t)
-        (princ (format "  This variable is an alias for `%s'.\n" alias)))
-
-      (when obsolete
-        (setq extra-line t)
-        (princ "  This variable is obsolete")
+    ;; Indicate ???
+    (when obsolete
+      (let ((use (car obsolete)))
+        (princ "\n  This variable is obsolete")
         (if (nth 2 obsolete)
-            (princ (format " since %s" (nth 2 obsolete))))
-        (princ (cond ((stringp use) (concat ";\n  " use))
-                     (use (format ";\n  use `%s' instead." (car obsolete)))
-                     (t ".")))
-        (terpri))
-
-      (when (member (cons variable val) file-local-variables-alist)
-        (setq extra-line t)
-        (if (member (cons variable val) dir-local-variables-alist)
-            (let ((file (and (buffer-file-name)
-                             (not (file-remote-p (buffer-file-name)))
-                             (dir-locals-find-file
-                              (buffer-file-name))))
-                  (dir-file t))
-              (princ "  This variable's value is directory-local")
-              (if (null file)
-                  (princ ".\n")
-                (princ ", set ")
-                (if (consp file) ; result from cache
-                    ;; If the cache element has an mtime, we
-                    ;; assume it came from a file.
-                    (if (nth 2 file)
-                        (setq file (expand-file-name
-                                    dir-locals-file (car file)))
-                      ;; Otherwise, assume it was set directly.
-                      (setq dir-file nil)))
-                (princ (if dir-file
-                           "by the file\n  `"
-                         "for the directory\n  `"))
-                (with-current-buffer standard-output
-                  (insert-text-button
-                   file 'type 'help-dir-local-var-def
-                   'help-args (list variable file))
-                  )
-                (princ "'.\n")))
-          (princ "  This variable's value is file-local.\n")))
-
-      (when (memq variable ignored-local-variables)
-        (setq extra-line t)
-        (princ "  This variable is ignored as a file-local \
-variable.\n"))
-
-      ;; Can be both risky and safe, eg auto-fill-function.
-      (when (risky-local-variable-p variable)
-        (setq extra-line t)
-        (princ "  This variable may be risky if used as a \
-file-local variable.\n")
-        (when (assq variable safe-local-variable-values)
-          (princ "  However, you have added it to \
-`safe-local-variable-values'.\n")
+            (princ (format " since %s" (nth 2 obsolete)))
           )
-        )
-
-      (when safe-var
-        (setq extra-line t)
-        (princ "  This variable is safe as a file local variable ")
-        (princ "if its value\n  satisfies the predicate ")
-        (princ (if (byte-code-function-p safe-var)
-                   "which is a byte-compiled expression.\n"
-                 (format "`%s'.\n" safe-var)))
-        )
-
-      (if extra-line (terpri))
-      (princ "Documentation:\n")
-      (with-current-buffer standard-output
-        (insert (or doc "Not documented as a variable."))
+        (princ (cond 
+                ((stringp use)
+                 (concat "; " use)
+                 )
+                (use
+                 (format "; use `%s' instead." (car obsolete))
+                 )
+                (t ".")
+                ))
         )
       )
 
-
-    ;; Make a link to customize if this variable can be customized.
-    (when (custom-variable-p variable)
-      (let ((customize-label "customize"))
-        (terpri)
-        (terpri)
-        (princ (concat "You can " customize-label " this variable."))
-        (with-current-buffer standard-output
-          (save-excursion
-            (re-search-backward
-             (concat "\\(" customize-label "\\)") nil t)
-            (help-xref-button 1 'help-customize-variable variable)
+    ;; Indicate ???
+    (when (member (cons variable val) file-local-variables-alist)
+      (if (member (cons variable val) dir-local-variables-alist)
+          ;; dir-local:
+          (let ((file (and (buffer-file-name)
+                           (not (file-remote-p (buffer-file-name)))
+                           (dir-locals-find-file
+                            (buffer-file-name))))
+                (dir-file t))
+            (princ "\n  This variable's value is directory-local")
+            (unless (null file)
+              (princ ", set ")
+              (if (consp file) ; result from cache
+                  ;; If the cache element has an mtime, we
+                  ;; assume it came from a file.
+                  (if (nth 2 file)
+                      (setq file (expand-file-name
+                                  dir-locals-file (car file)))
+                    ;; Otherwise, assume it was set directly.
+                    (setq dir-file nil)
+                    )
+                )
+              ;; TODO: There was a xref-button, something is missing.
+              (princ (if dir-file
+                         "by the file."
+                       "for the directory."))
+              )
             )
-          )
-        )
-      ;; Note variable's version or package version
-      (let ((output (describe-variable-custom-version-info variable)))
-        (when output
-          (terpri)
-          (terpri)
-          (princ output)
-          )
+        ;; file-local:
+        (princ "\n  This variable's value is file-local.")
         )
       )
 
-    ;; (with-current-buffer standard-output
-    ;;   ;; Return the text we displayed.
-    ;;   (buffer-string)
-    ;;   )
+    ;; Indicate ???
+    (when (memq variable ignored-local-variables)
+      (princ "\n  This variable is ignored as a file-local variable."))
+
+    ;; Indicate it could be both risky and safe, eg auto-fill-function.
+    (when (risky-local-variable-p variable)
+      (princ "\n  This variable may be risky if used as a file-local variable.")
+      (when (assq variable safe-local-variable-values)
+        (princ "\n  However, you have added it to `safe-local-variable-values'.")
+        )
+      )
+
+    ;; Indicate ???
+    (when safe-var
+      (princ "\n  This variable is safe as a file local variable.")
+      (princ "\n  If its value, satisfies the predicate ")
+      (princ (if (byte-code-function-p safe-var)
+                 "which is a byte-compiled expression."
+               (format "`%s'." safe-var)))
+      )
+
+    ;; Documentation.
+    (terpri)
+    (terpri)
+    (princ "Documentation:\n")
+    (princ (or doc "Not documented as a variable."))
     )
+
+  ;; Make a link to customize if this variable can be customized.
+  ;; (when (custom-variable-p variable)
+  ;;   (let ((customize-label "customize"))
+  ;;     (terpri)
+  ;;     (terpri)
+  ;;     (princ (concat "You can " customize-label " this variable."))
+  ;;     (with-current-buffer standard-output
+  ;;       (save-excursion
+  ;;         (re-search-backward
+  ;;          (concat "\\(" customize-label "\\)") nil t)
+  ;;         (help-xref-button 1 'help-customize-variable variable)
+  ;;         )
+  ;;       )
+  ;;     )
+  ;;   ;; Note variable's version or package version
+  ;;   (let ((output (describe-variable-custom-version-info variable)))
+  ;;     (when output
+  ;;       (terpri)
+  ;;       (terpri)
+  ;;       (princ output)
+  ;;       )
+  ;;     )
+  ;;   )
   )
--- oops/lang/oops-lisp-lib.el
+++ oops/lang/oops-lisp-lib.el
@@ -147,8 +147,6 @@
     )
   )
 
-;; (oops--lisp-describe-function 'subr-arity)
-;; standard-output
 (defun oops--lisp-describe-function (function)
   "Display the full documentation of FUNCTION (a symbol)."
   (let ((standard-output (oops--lisp-help-buffer 1)))
@@ -161,8 +159,230 @@
     )
   )
 
-;; (find-lisp-object-file-name 'standard-output 'defvar)
-;; (string-match "[.]\\(c\\|cpp\\)$" (find-lisp-object-file-name 'standard-output 'defvar))
+;; (oops--lisp-describe-variable 'standard-output)
+(defun oops--lisp-describe-variable (variable)
+  (let* ((standard-output (oops--lisp-help-buffer 1))
+         (alias (condition-case nil
+                    (indirect-variable variable)
+                  (error variable)))
+         (doc (or (documentation-property
+                   variable 'variable-documentation)
+                  (documentation-property
+                   alias 'variable-documentation)))
+         (val (symbol-value variable))
+         (locus (variable-binding-locus variable))
+         (file-name (find-lisp-object-file-name variable 'defvar))
+         (permanent-local (get variable 'permanent-local))
+         (obsolete (get variable 'byte-obsolete-variable))
+         (safe-var (get variable 'safe-local-variable))
+         (print-length 32)
+         (print-level 3)
+         )
+    ;; TODO: Make variable text with hyper-link property.
+
+    ;; Indicate where is its definition and current value.
+    (princ (format "`%s'" variable))
+    (princ " is a variable defined in `")
+    (princ (if (eq file-name 'C-source)
+               "C source code"
+             (file-name-nondirectory file-name)))
+    (princ "'.")
+    (terpri)
+    (terpri)
+    (princ "Its value is `")
+    (pp val)
+    (princ "'.")
+    ;; Add hyper-link property to file-name text.
+    ;; (with-current-buffer standard-output
+    ;;   (save-excursion
+    ;;     (re-search-backward "`\\([^`']+\\)'" nil t)
+    ;;     (help-xref-button 1 'help-variable-def
+    ;;                       variable file-name)
+    ;;     )
+    ;;   )
+
+    ;; If it is a customizable variable, show its original value.
+    ;; "Original value was?"
+    (let* ((sv (get variable 'standard-value))
+           (origval (and (consp sv)
+                         (condition-case nil
+                             (eval (car sv))
+                           (error :help-eval-error)))))
+      (when (and (consp sv)
+                 (not (equal origval val))
+                 (not (equal origval :help-eval-error)))
+        (princ "\ncustomizable, original value was `")
+        (pp origval)
+        (princ "'.")
+        )
+      )
+
+    ;; Indicate which kind of variable it is and its defaulte value.
+    ;; "Local in buffer?"
+    ;; "Frame-local variable?"
+    ;; "Terminal-local variable?"
+    (when locus
+      (cond
+       ((bufferp locus)
+        (princ (format "\nlocal in buffer `%s'." (buffer-name)))
+        )
+       ((framep locus)
+        (princ (format "\nframe-local variable."))
+        )
+       ((terminal-live-p locus)
+        (princ (format "\nterminal-local variable."))
+        )
+       (t (princ (format "\nit is local to %S." locus)))
+       )
+
+      (if (not (default-boundp variable))
+          (princ "\nglobally void")
+        (let ((global-val (default-value variable)))
+          (princ "\nglobal value is ")
+          (if (eq val global-val)
+              (princ "the same.")
+            (princ "`")
+            (pp global-val)
+            (princ "'.")
+            )
+          )
+        )
+      )
+
+    ;; Indicate which kind of local variable it is.
+    (terpri)
+    (cond
+     ((and (local-variable-if-set-p variable)
+           (or (not (local-variable-p variable))
+               (with-temp-buffer
+                 (local-variable-if-set-p variable))))
+      (princ "\n  Automatically becomes ")
+      (if permanent-local
+          (princ "permanently "))
+      (princ "buffer-local when set.")
+      )
+     ((not permanent-local))
+     ((bufferp locus)
+      (princ "\n  This variable's buffer-local value is permanent.")
+      )
+     (t
+      (princ "\n  This variable's value is permanent if it is given a local binding."))
+     )
+
+    ;; Indicate it is a alias.
+    (unless (eq alias variable)
+      (princ (format "\n  This variable is an alias for `%s'." alias))
+      )
+
+    ;; Indicate ???
+    (when obsolete
+      (let ((use (car obsolete)))
+        (princ "\n  This variable is obsolete")
+        (if (nth 2 obsolete)
+            (princ (format " since %s" (nth 2 obsolete)))
+          )
+        (princ (cond 
+                ((stringp use)
+                 (concat "; " use)
+                 )
+                (use
+                 (format "; use `%s' instead." (car obsolete))
+                 )
+                (t ".")
+                ))
+        )
+      )
+
+    ;; Indicate ???
+    (when (member (cons variable val) file-local-variables-alist)
+      (if (member (cons variable val) dir-local-variables-alist)
+          ;; dir-local:
+          (let ((file (and (buffer-file-name)
+                           (not (file-remote-p (buffer-file-name)))
+                           (dir-locals-find-file
+                            (buffer-file-name))))
+                (dir-file t))
+            (princ "\n  This variable's value is directory-local")
+            (unless (null file)
+              (princ ", set ")
+              (if (consp file) ; result from cache
+                  ;; If the cache element has an mtime, we
+                  ;; assume it came from a file.
+                  (if (nth 2 file)
+                      (setq file (expand-file-name
+                                  dir-locals-file (car file)))
+                    ;; Otherwise, assume it was set directly.
+                    (setq dir-file nil)
+                    )
+                )
+              ;; TODO: There was a xref-button, something is missing.
+              (princ (if dir-file
+                         "by the file."
+                       "for the directory."))
+              )
+            )
+        ;; file-local:
+        (princ "\n  This variable's value is file-local.")
+        )
+      )
+
+    ;; Indicate ???
+    (when (memq variable ignored-local-variables)
+      (princ "\n  This variable is ignored as a file-local variable."))
+
+    ;; Indicate it could be both risky and safe, eg auto-fill-function.
+    (when (risky-local-variable-p variable)
+      (princ "\n  This variable may be risky if used as a file-local variable.")
+      (when (assq variable safe-local-variable-values)
+        (princ "\n  However, you have added it to `safe-local-variable-values'.")
+        )
+      )
+
+    ;; Indicate ???
+    (when safe-var
+      (princ "\n  This variable is safe as a file local variable.")
+      (princ "\n  If its value, satisfies the predicate ")
+      (princ (if (byte-code-function-p safe-var)
+                 "which is a byte-compiled expression."
+               (format "`%s'." safe-var)))
+      )
+
+    ;; Documentation.
+    (terpri)
+    (terpri)
+    (princ "Documentation:\n")
+    (princ (or doc "Not documented as a variable."))
+
+    ;; Make a link to customize if this variable can be customized.
+    ;; (when (custom-variable-p variable)
+    ;;   (let ((customize-label "customize"))
+    ;;     (terpri)
+    ;;     (terpri)
+    ;;     (princ (concat "You can " customize-label " this variable."))
+    ;;     (with-current-buffer standard-output
+    ;;       (save-excursion
+    ;;         (re-search-backward
+    ;;          (concat "\\(" customize-label "\\)") nil t)
+    ;;         (help-xref-button 1 'help-customize-variable variable)
+    ;;         )
+    ;;       )
+    ;;     )
+    ;;   ;; Note variable's version or package version
+    ;;   (let ((output (describe-variable-custom-version-info variable)))
+    ;;     (when output
+    ;;       (terpri)
+    ;;       (terpri)
+    ;;       (princ output)
+    ;;       )
+    ;;     )
+    ;;   )
+
+    (with-current-buffer standard-output
+      (goto-char 1)
+      )
+    )
+  )
+
 (defun oops-lisp-show-help-atpt ()
   (let* ((symb (intern-soft (oops--lisp-thingatpt))))
     (when symb
@@ -176,7 +396,11 @@
         (let ((fname (find-lisp-object-file-name symb 'defvar)))
           (if (or (eq fname 'C-source)
                   (string-match "[.]\\(c\\|cpp\\)$" fname))
-              (message "Yet support for built-in variable: %s." symb)
+              ;; (message "Yet support for built-in variable: %s." symb)
+              (progn
+                (oops--lisp-describe-variable symb)
+                (oops-update-help (oops--lisp-help-buffer))
+                )
             ;; Try not to use `find-variable-noselect'.
             (oops-update-help (save-excursion
                                 (find-variable-noselect symb)))
