1d5dee6a9c9ffd5808674b1fc2cd459ee6665b8c
Boy.Wang <boyw165@gmail.com>
2014-07-06 10:33:29 +0800

    no message

--- oops.el
+++ oops.el
@@ -100,6 +100,35 @@
     )
   )
 
+;; (defun paradox-require (feature &optional filename noerror package refresh)
+;;   "A replacement for `require' which also installs the feature if it is absent.
+;; - If FEATURE is present, `require' it and return t.
+
+;; - If FEATURE is not present, install PACKAGE with `package-install'.
+;; If PACKAGE is nil, assume FEATURE is the package name.
+;; After installation, `require' FEATURE.
+
+;; FILENAME is passed to `require'.
+
+;; If NOERROR is non-nil, don't complain if the feature couldn't be
+;; installed, just return nil.
+
+;; By default, the current package database (stored in
+;; `package-archive-contents') is only updated if it is empty.
+;; Passing a non-nil REFRESH argument forces this update."
+;;   (or (require feature filename t)
+;;       (let ((package (or package
+;;                          (if (stringp feature)
+;;                              (intern feature)
+;;                            feature))))
+;;         (require 'package)
+;;         (unless (and package-archive-contents (null refresh))
+;;           (package-refresh-contents))
+;;         (and (condition-case e
+;;                  (package-install package)
+;;                (error (if noerror nil (error (cadr e)))))
+;;              (require feature filename noerror)))))
+
 ;;; Text Operation =============================================================
 
 ;;;###autoload
