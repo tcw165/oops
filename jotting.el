ab18cd52f4675bb1115aeee83edafc589ae18468
Boy.Wang <boyw165@gmail.com>
2014-10-25 23:19:33 +0800

    - [prj] Add Shell document type.

--- jotting.el
+++ jotting.el
@@ -1,9 +1,10 @@
-(require 'grizzl)
-(grizzl-completing-read "> " (grizzl-make-index '("/Users/Boy/loopy/lib/python2.7/site-packages/atom/enum.py"
-                                                  "/Users/Boy/loopy/lib/python2.7/site-packages/atom/intenum.py"
-                                                  "/Users/Boy/Documents/_CPP/ycmd/cpp/ycm/Utils.cpp"
-                                                  "/Users/Boy/Documents/_CPP/ycmd/ycmd/completers/general/tests/testdata/filename_completer/test.cpp")))
-(grizzl-result-count prj-total-files-cache)
-(hash-table-count prj-total-files-cache)
+;; (require 'grizzl)
+;; (grizzl-completing-read "> " (grizzl-make-index '("/Users/Boy/loopy/lib/python2.7/site-packages/atom/enum.py"
+;;                                                   "/Users/Boy/loopy/lib/python2.7/site-packages/atom/intenum.py"
+;;                                                   "/Users/Boy/Documents/_CPP/ycmd/cpp/ycm/Utils.cpp"
+;;                                                   "/Users/Boy/Documents/_CPP/ycmd/ycmd/completers/general/tests/testdata/filename_completer/test.cpp")))
 
-(grizzl-mode)
+(require 'jedi)
+(add-hook 'python-mode-hook 'jedi:setup)
+(setq jedi:complete-on-dot t)
+
--- prj/prj.el
+++ prj/prj.el
@@ -88,7 +88,7 @@ project; Powerful selective grep string or regular expression in a project, etc.
 
 (defcustom prj-document-types '(("Text" . "*.txt;*.md")
                                 ("Emacs Lisp" . ".emacs;*.el")
-                                ("Shell" . "*.sh")
+                                ("Shell" . ".bashrc;.zshrc;*.sh")
                                 ("Python" . "*.py")
                                 ("C/C++ Header" . "*.h;*.hxx;*.hpp")
                                 ("C/C++ Source" . "*.c;*.cxx;*.cpp"))
